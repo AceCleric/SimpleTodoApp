af7204d86c4df73c3d3b85a01c132da82d6a3588
AceCleric <anouar_azzouz@outlook.com>
2023-07-03 21:18:34 +0200

    Add project files.

---
+++ SimpleTodoApp/Program.cs
@@ -0,0 +1,80 @@
+﻿// This is a simple Todo application
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Globalization;
+
+namespace SimpleTodoApp
+{
+   public class Program
+    {
+        static void Main()
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Welcome to the simple Todo application!");
+
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("\ts - Show todos");
+                Console.WriteLine("\tc - Create todo");
+
+                string userActionInput = Console.ReadLine();
+
+                var ProgramInstance = new Program();
+                ProgramInstance.processUserAction(userActionInput);
+
+                Console.WriteLine("Would you like to close the Todos app? Yes or No");
+                if (Console.ReadLine().ToLower() != "yes")
+                {
+                    continue;
+                }
+                break;
+            }
+        }
+
+        private void processUserAction(string input)
+        {
+            if (input == "s")
+            {
+                Console.WriteLine("Here are all your todos");
+
+                Console.WriteLine("Would you like to perform an action? Yes or No");
+                string performActionAnswer = Console.ReadLine().ToLower();
+
+                if (performActionAnswer == "yes")
+                {
+                    Console.WriteLine("\tu - Which todo would you like to pick?");
+
+                    Console.WriteLine("\tu - Update todo");
+                    Console.WriteLine("\tf - Finish todo");
+                    Console.WriteLine("\td - Delete todo");
+
+                }
+
+            }
+            else if (input == "c")
+            {
+                Console.WriteLine("Let's Create your todo!");
+
+                Console.WriteLine("What is the title of your todo?");
+                string title = Console.ReadLine();
+
+                Console.WriteLine("What is the description of your todo?");
+                string description = Console.ReadLine();
+
+                List<Todo> todos = new List<Todo>();
+                todos.Add(new Todo { Title = title, Description = description });
+
+                TodoManager todoManager = new TodoManager();
+                todoManager.SaveTodos(todos);
+
+        
+            }
+            else
+            {
+                Console.WriteLine("Invalid option inserted! Please try again!");
+            }
+        }
+    }
+}
---
+++ SimpleTodoApp/TodoManager.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SimpleTodoApp
+{
+
+    public class TodoManager
+    {
+
+        private static string GetProjectRootDirectory()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+
+            while (!string.IsNullOrEmpty(currentDirectory))
+            {
+                string[] files = Directory.GetFiles(currentDirectory, "*.csproj");
+
+                if (files.Length > 0)
+                {
+                    return currentDirectory;
+                }
+
+                currentDirectory = Directory.GetParent(currentDirectory)?.FullName;
+            }
+
+            throw new Exception("Project root directory not found.");
+        }
+
+
+        public void SaveTodos(List<Todo> todos)
+        {
+            string json = JsonConvert.SerializeObject(todos, Formatting.Indented);
+            string projectRootDirectory = GetProjectRootDirectory();
+            string filePath = Path.Combine(projectRootDirectory, "todos.json");
+
+            File.WriteAllText(filePath, json);
+            Console.WriteLine("File saved successfully.");
+            Console.WriteLine("Current Directory: " + Directory.GetCurrentDirectory());
+        }
+    }
+
+}
